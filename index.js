1ee01a842b089cae100130ff5b830f360ec2a676
Isaac <isaacseligcoding@gmail.com>
2021-04-15 11:53:05 +0100

    updated methods

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const SQL = require("./lib/sql");
 console.log(`Hello world, gonna try some database stuff`);
 
 const db = new SQL("127.0.0.1", "root", "PASSWORD", "cactus");
-db.select((error, results) => {
+/*db.select((error, results) => {
   if (error) {
     throw error;
   } else {
@@ -13,3 +13,31 @@ db.select((error, results) => {
     process.exit();
   }
 });
+*/
+
+/*db.create(
+  (error, results) => {
+    if (error) throw error;
+
+    console.log(results);
+  },
+  [`1000`, `Developer`, 20991232, 20991252]
+);
+*/
+
+/*db.update(
+  (error, results) => {
+    if (error) throw error;
+
+    console.log(results);
+  },
+  `Senior Developer`,
+  1002
+);
+*/
+
+db.delete((error, results) => {
+  if (error) throw error;
+
+  console.log(results);
+}, 1002);
